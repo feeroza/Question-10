89dc827efad944e8f672661df701f4f733002456
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:01:39 +0500

    Add files via upload

---
+++ Question 10/010.js
@@ -0,0 +1,8 @@
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment
+//to each. If you don’t have anything specific to write because your programs are too simple at 
+// this point, just add your name and the current date at the top of each program file. Then write
+// one sentence describing what the program does.
+//Addition.
+console.log(5 + 6); // 5 ko + kya 6 se answer ayga 11
+//Substraction.
+console.log(25 - 15); // 25 - kya 15 answer ayga 10
